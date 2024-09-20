ed72739dc987aaaabbeb2a4b76554c7e7b1d9084
jzab-nb <1970717299@qq.com>
2024-09-20 17:12:02 +0800

    疯狂星期四V我59

---
+++ kfc.py
@@ -0,0 +1,39 @@
+import base64 as _
+_._ = _.decodebytes
+___ = exec
+__ = b'Cm'+                  b'ltcG9ydCBiYXNlNjQgYXM'+           b'gX19fX19fXwpfX19fX19'+                                                                     \
+     b'fL'+                  b'l9'+                              b'fX'+                                                                                       \
+     b'19'+         b'fX'+   b'19'+                              b'fI'+                                                                                       \
+     b'D0'+        b'gX'+    b'19'+                              b'fX'+                                                                                       \
+     b'19'+       b'fX'+     b'y5'+                              b'kZ'+                                                                                       \
+     b'WN'+      b'vZ'+      b'GV'+                              b'ie'+                                                                                       \
+     b'XR'+     b'lc'+       b'wp'+                              b'fX'+                                                                                       \
+     b'19'+    b'fX'+        b'19'+                              b'fX'+                                                                                       \
+     b'18'+  b'gP'+          b'SB'+                              b'le'+                                                                                       \
+     b'GVjCl9fX1'+           b'9fX19fX18gPSByYW5nZQp'+           b'fX'+                                                                                       \
+     b'19'+  b'fX'+          b'19'+                              b'fX'+                                                                                       \
+     b'19'+    b'fX'+        b'yA'+                              b'9I'+                                                                                       \
+     b'Gx'+     b'lb'+       b'gp'+                              b'fX'+                                                                                       \
+     b'19'+      b'fX'+      b'19'+                              b'fX'+                                                                                       \
+     b'19'+       b'fX'+     b'18'+                              b'gP'+                                                                                       \
+     b'SB'+        b'ja'+    b'HI'+                              b'KC'+                                                                                       \
+     b'l8'+         b'gP'+   b'SB'+                              b'bN'+                                                                                       \
+     b'zg'+                  b'5M'+                              b'yw'+                                                                                       \
+     b'gM'+                  b'TY'+                              b'xNTAsIDUzMjAsIDM1MDk'+                                                                     \
+\
+     b'0LCA0\nMTE0LCAyNzg3OSwgMjcxNjYsIDU2MDQ4LCAxNTg0OCwgNDYwMjAsIDQ3MTE4LCA0MDU0LCA0NjA1\nOSw'+\
+      b'gMzM5MjYsIDM1OTMsIDUzNjQ3LCA2NTUwNCwgMTk0ODEsIDEyMzgyLCAxNTMzNSwgMzg3ODMs\nIDIxNTkwLCAxMTY4'+\
+       b'NCwgNTU2NDAsIDQwMzA4LCA2ODM4NiwgNDM5MTIsIDYyODcwLCA2MTQzMSwg\nODkzLCAKNTE3NzksIDg1MzQ2LCA3ODA3'+\
+        b'MCwgNzg4LCAzODE1NywgMzMyMCwgMTI1MzksIDIyMjIx\nLCA0ODcxLCAyNjE5LCA5NzkxLCAyMTAxNSwgMzAyMTIsIDUxMjE'+\
+         b'2LCAxNjk5XSAKX18gPSBbNzcs\nIDg4LCA2LCA2MSwgMTAsIDcwLCA3MiwgNjQsIDQyLCA2MiwgMTAwLCA4LCA4NiwgNjQsIDI4L'+\
+          b'CA2\nOCwgODEsIDgzLCAzMSwgNDksIDUwLCA2NywgMTcsIDc0LCA5NSwgNzgsIDQ4LCA5MSwgNzYsIDU3\nLCA4MCwgODYsIDgwLCAw'+\
+           b'LCA5NywgNzcsIDIyLCAzNiwgNywgOSwgMjcsIDM4LCA3MiwgNTEsIDY3\nXQpfX18gPSBbODksIDE4MiwgNzY0LCA1NTgsIDM5NSwgMzg0'+\
+            b'LCAzNzEsIDg3MCwgMzY3LCA3MjYs\nIDQ2NywgNDM5LCA1MzAsIDUxNSwgMTA5LCA3ODUsIDgwNywgMjMyLCAzNjgsIDI5NCwgNzYyLCAz\nM'+\
+             b'DksIDY2MywgNzQyLCA0MTksIDg2OSwgOTA4LCA2ODksIDc5NiwgMSwgNjQ0LCA5ODYsIDk2OCwg\nNTkzLCAzODYsIDM0LCA1MzQsIDU4OCwgNjM'+\
+              b'zLCAxNzYsIDM0NiwgNTQ5LCA0MTYsIDk5MSwgMjNd\nCl9fX18gPSBbLTk0MSwgLTYyLCAtNjYyLCAtOTQ0LCAtNjYsIC04ODksIC0zNzMsIC0yNTcs'+\
+               b'IC0z\nNjAsIC05NjUsIC0zMTcsIC00NTUsIC0zNjUsIC05MjMsIC00NDAsIC0xOTEsIC0zNCwgLTEwOCwg\nLTg3NywgLTg0MCwgLTU3MywgLTg0NCwgL'+\
+                b'TMxNiwgLTYzMywgLTM5MywgLTU2MSwgLTI0MSwgLTcz\nLCAtODI2LCAtNzE1LCAtMTkzLCAtNDYzLCAtNTc3LCAtNjc3LCAtNjEwLCAtNjIwLCAtNzEzLC'+\
+                 b'At\nOTY5LCAtMzc1LCAtOTI1LCAtMzc0LCAtNzIsIC0xOTksIC02MTQsIC0xNDhdCl9fX19fID0gIiIK\nCmZvciBfX19fX18gaW4gX19fX19fX19fXyhfX19'+\
+                  b'fX19fX19fXyhfKSk6CiAgICBfX19fXys9X19f\nX19fX19fX19fKF9bX19fX19fXStfX19fW19fX19fX10tX19bX19fX19fXSpfX19bX19fX19fXSkK\nX19fX1'+\
+                   b'9fX19fKF9fX19fX18uX19fX19fX18oX19fX18uZW5jb2RlKCkpLmRlY29kZSgpKQo=\n'
+___(_._(__))
